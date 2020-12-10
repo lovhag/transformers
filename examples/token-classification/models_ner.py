de2d390592e50b19a78296abf70213a03fa052df
Lovisa Hagström <lovhagstrom@gmail.com>
2020-12-10 12:38:30 +0100

    Add support for training with more models than BERT.

---
+++ examples/token-classification/models_ner.py
@@ -0,0 +1,39 @@
+# MODEL CLASSES!
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
+
+class SimpleClassifier(nn.Module):
+
+    def __init__(self, config):        
+        super().__init__()
+        self.classifier = nn.Linear(config.max_seq_length, len(config.id2label))
+        
+    def forward(self, input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+    ):
+        logits = self.classifier(input_ids)
+
+        outputs = (logits,)
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)
+                active_labels = torch.where(
+                    active_loss, labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(labels)
+                )
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), scores
